a8065053f1137b70bb7722075dcb3ec7d1174962
Agustin <aguvargass3@gmail.com>
2023-11-14 22:08:24 -0300

    portfolio 99%

--- script.js
+++ script.js
@@ -1,41 +1,54 @@
 const menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
+
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
+    } else {
+        document.getElementById("nav").classList = "responsive";
         menuVisible = true;
     }
 }
 
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
+function seleccionar() {
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
+
+function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
+    if (distancia_skills >= 300) {
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
+        habilidades[2].classList.add("database");
         habilidades[3].classList.add("react");
         habilidades[4].classList.add("node");
         habilidades[5].classList.add("comunicacion");
         habilidades[6].classList.add("trabajo");
         habilidades[7].classList.add("creatividad");
         habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("iniciativa");
+        habilidades[9].classList.add("ingles");
     }
 }
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
+window.onscroll = function () {
     efectoHabilidades();
-} 
+
+    // Código para el botón de regreso arriba
+    var botonArriba = document.querySelector('.arriba');
+
+    if (window.scrollY > 300) {
+        botonArriba.style.display = 'block';
+    } else {
+        botonArriba.style.display = 'none';
+    }
+    
+    // Agregar listener para el clic en el botón de regreso arriba
+    botonArriba.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+};
